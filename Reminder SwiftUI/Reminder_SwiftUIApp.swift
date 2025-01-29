2eb48a10b6418f1ed80336ccf31ef5b4420276dd
larissa.silva <larissa.silva@sejaefi.com.br>
2025-01-29 19:48:55 -0300

    Initial Commit

---
+++ Reminder SwiftUI/Reminder_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminder_SwiftUIApp.swift
+//  Reminder SwiftUI
+//
+//  Created by Larissa Silva | Efí on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Reminder_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
